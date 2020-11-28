ae76268c6b992ebbdddde63a26a66aaf1e461c82
svaidya <svaidya401@gmail.com>
2020-11-28 16:50:46 +0530

    added user info endpoint

--- legion/user_app/urls.py
+++ legion/user_app/urls.py
@@ -3,6 +3,7 @@ from user_app import views
 
 app_name = 'user_app'
 urlpatterns = [
+    path('info', views.user_info, name='info'),
     path('login', views.user_login, name='login'),
     path('register', views.register, name='register'),
     path('logout', views.user_logout, name='logout'),
